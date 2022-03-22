1dbb6fc1b38ca260c81882de74000a0ea4d74a42
Josckar <Josckarpalo21@hotmail.com>
2022-03-21 22:57:17 -0400

    vzdfsfz

---
+++ src/project3/PossibleLocationsQueue.java
@@ -0,0 +1,23 @@
+package project3;
+
+public class PossibleLocationsQueue implements PossibleLocations {
+
+	@Override
+	public void add(Location s) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Location remove() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
