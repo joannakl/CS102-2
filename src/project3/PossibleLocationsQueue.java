ea3f621c0cad99f19b7e98c7a9cd4b1bb950e528
Josckar <Josckarpalo21@hotmail.com>
2022-04-04 14:21:07 -0400

    dsanjkdsanljks

--- src/project3/PossibleLocationsQueue.java
+++ src/project3/PossibleLocationsQueue.java
@@ -2,22 +2,72 @@ package project3;
 
 public class PossibleLocationsQueue implements PossibleLocations {
 
-	@Override
-	public void add(Location s) {
-		// TODO Auto-generated method stub
-		
+	private static int frontIndex, backIndex, capacity;
+	private static Location[] locations;
+	
+	/**
+	 * 
+	 * @param c
+	 */
+	PossibleLocationsQueue(int totalCapacity) {
+		frontIndex = 0;
+		backIndex = 0;
+		capacity = totalCapacity;
+		locations = new Location[capacity];
 	}
 
-	@Override
+    /**
+    * Add a Location object to this collection.
+    * @param s object to be added
+    * @throws NullPointerException if the given location is null
+    */
+	public void add(Location s) throws NullPointerException {
+		// check locations is full or not
+		if (s == null) {
+			throw new NullPointerException("Location is null");
+		}
+		// insert element at the backIndex
+		else {
+			locations[backIndex] = s;
+			backIndex++;
+		}
+		return;
+	}
+
+    /**
+     * Remove the next object from this collection. The specific
+     * item returned is determined by the underlying structure
+     * by which this collection is represented.
+     * @return the next object, or null if set is empty
+     */
 	public Location remove() {
-		// TODO Auto-generated method stub
-		return null;
+		// return null if the set is empty
+		if (this.isEmpty()) {
+			return null;
+		}
+		
+		else {
+			for (int i = frontIndex; i < backIndex - 1; i++) {
+				locations[i] = locations[i + 1];
+			}
+
+			// store 0 at backIndex indicating there's no element
+			if (backIndex < capacity)
+				locations[backIndex] = null;
+
+			// decrement backIndex
+			backIndex--;
+		}
+		return locations[frontIndex];
 	}
 
-	@Override
+	
+    /**
+     * Determines if this collection is empty or not.
+     * @return  true, if set is empty, false, otherwise.
+     */
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
+		return(frontIndex == backIndex ? true: false);
 	}
 
 }
--- src/project3/PossibleLocationsStack.java
+++ src/project3/PossibleLocationsStack.java
@@ -5,53 +5,33 @@ import java.util.LinkedList;
 class PossibleLocationsStack implements PossibleLocations {
 	
 	Node top;
-
-    // A linked list node
 	private class Node {
+		Location data; 
+		Node link;
+	}
 
-		Location data; // integer data
-		Node link; // reference variable Node type
-		}
-	// create global top reference variable global
-	
-	
-	
-	// Constructor
-	PossibleLocationsStack() {
+	public PossibleLocationsStack() {
 		this.top = null;
 	}
-	 
-	// Utility function to add an element x in the stack
-	public void add(Location s) { // insert at the beginning
 
-		// create new node temp and allocate memory
+	public void add(Location s) {
+
 		Node temp = new Node();
 
-		// check if stack (heap) is full. Then inserting an
-		//  element would lead to stack overflow
-		if (temp == null) {
-			System.out.print("\nHeap Overflow");
-			return ;
-		}
-
-		// initialize data into temp data field
 		temp.data = s;
 
-		// put top reference into temp link
 		temp.link = top;
 
-		// update top reference
+
 		top = temp;
 	}
 
-	// Utility function to check if the stack is empty or not
 	public boolean isEmpty() {
 		return top == null;
 	}
 
-	// Utility function to return top element in a stack
 	public Location peek() {
-		// check for empty stack
+
 		if (!isEmpty()) {
 			return top.data;
 		}
@@ -62,16 +42,14 @@ class PossibleLocationsStack implements PossibleLocations {
 		}
 	}
 
-	// Utility function to pop top element from the stack
-	public Location remove() { // remove at the beginning
+	public Location remove() { 
 		Node temp = new Node();
-		// check for stack underflow
+	
 		if (top == null) {
 			System.out.print("\nStack Underflow");
 			return null;
 		}
 
-		// update the top pointer to point to the next node
 		temp.data = top.data;
 		temp.link = top.link;
 		top = (top).link;
@@ -79,7 +57,7 @@ class PossibleLocationsStack implements PossibleLocations {
 	}
 
 	public void display() {
-		// check for stack underflow
+		
 		if (top == null) {
 			System.err.print("Top is null");
 			System.exit(0);
@@ -88,10 +66,8 @@ class PossibleLocationsStack implements PossibleLocations {
 			Node temp = top;
 			while (temp != null) {
 
-				// print node data
 				System.out.printf("%d->", temp.data);
 
-				// assign temp link to temp
 				temp = temp.link;
 			}
 		}
